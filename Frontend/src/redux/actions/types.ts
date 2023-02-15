d8c94476c1defc6dbe03bd623b6f3cd99fed6dff
libinleo <70009634+libinleo@users.noreply.github.com>
2023-02-15 17:34:40 +0530

    Delete Frontend directory

--- Frontend/src/redux/actions/types.ts
+++
@@ -1,2 +0,0 @@
-export const UPDATE_COUNT = 'UPDATE_COUNT';
-export const API_CALL = 'API_CALL';
--- Frontend/src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import commonReducer from './common';
-
-const RootReducer: any = combineReducers({
-    common: commonReducer,
-});
-
-export default RootReducer;
